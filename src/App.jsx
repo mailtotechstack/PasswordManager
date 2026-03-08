5eb43befa313a3e0d03ac66aef1bab3536fe77b2
mailtotechstack <mailtotechstack@gmail.com>
2026-03-08 13:48:56 +0530

    manager component case sensitive error resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Footer from "./components/Footer";
-import Manager from "./components/manager";
+import Manager from "./components/Manager";
 import Navbar from "./components/Navbar";
 
 export default function App(){
