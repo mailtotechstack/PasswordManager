c36cc5e3d241cbe0c78e53cf427c592dfad82310
mailtotechstack <mailtotechstack@gmail.com>
2026-03-09 18:11:27 +0530

    fix footer image isssue

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,10 @@
 const Footer = () => {
   return (
-    <footer className='bg-blue-950 text-white relative bottom-0 w-screen py-5'>
+    <footer className='bg-blue-950 text-white relative bottom-0 w-full py-5'>
       <div className='text-green-500 font-bold text-xl text-center'>
         &lt;<span className='text-white'>Pass</span>Word/&gt;
       </div>
-      <div className='flex justify-center items-center gap-2'>Created with <img className='h-5' src="heart.svg" alt="logo"/>by Mahi Kumar</div>
+      <div className='flex justify-center items-center gap-2'>Created with <img className='h-5' src="/heart.svg" alt="logo"/>by Mahi Kumar</div>
       
     </footer>
   )
