ca1d9c2eae21cc11340f92469a9672b7e3f6fa0f
mailtotechstack <mailtotechstack@gmail.com>
2026-03-09 18:07:13 +0530

    try to fix footer image issue

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
       <div className='text-green-500 font-bold text-xl text-center'>
         &lt;<span className='text-white'>Pass</span>Word/&gt;
       </div>
-      <div className='flex justify-center items-center gap-2'>Created with <img className='h-5' src="public/heart.svg" alt="logo"/>by Mahi Kumar</div>
+      <div className='flex justify-center items-center gap-2'>Created with <img className='h-5' src="heart.svg" alt="logo"/>by Mahi Kumar</div>
       
     </footer>
   )
